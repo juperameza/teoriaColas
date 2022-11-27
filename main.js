e4fc112e053629b1340550dfee6f75f12af3aef0
juperameza <juperameza@hotmail.com>
2022-11-26 22:20:18 -0600

    No se que hago xd|

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function calculateOneOnOne() {
   console.log(`Lq= ${(a * a) / (s * (s - a))}`);
   resultado.innerHTML = `lq= ${lq} wq= ${wq} ls= ${ls} ws= ${ws} u= ${u}`;
 }
+function alert() {
+  Swal.fire({
+    title: "Tienes los promedios de los datos?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.location.href = "oneOne.html";
+    } else {
+      document.location.href = "oneOne.html";
+    }
+  });
+}
