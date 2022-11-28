f878c9a97a2c90ff1c278b2babbac1343a2a59a3
juperameza <juperameza@hotmail.com>
2022-11-28 17:38:17 -0600

    Majo

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 function calculateOneOnOne() {
   let a = document.getElementById("tasaLlegadas").value;
   let s = document.getElementById("tasaServicio").value;
+  if (a > s) {
+    Swal.fire({
+      title:
+        "La tasa de promedio de llegadas no puede ser mayor que la tasa de promedio de servicio",
+      icon: "error",
+    });
+    return;
+  }
   let resultado = document.getElementById("result");
   let lq = Math.pow(a, 2) / (s * (s - a));
   let wq = a / (s * (s - a));
   let ls = a / (s - a);
   let ws = 1 / (s - a);
   let u = a / s;
-  console.log(`Lq= ${(a * a) / (s * (s - a))}`);
-  resultado.innerHTML = `lq= ${lq} wq= ${wq} ls= ${ls} ws= ${ws} u= ${u}`;
+  let results = {
+    lq: lq,
+    wq: wq,
+    ls: ls,
+    ws: ws,
+    u: u,
+  };
+  let text = "";
+  resultado.innerHTML = `<h2>Resultados</h2>`;
+  for (const property in results) {
+    switch (property) {
+      case "lq":
+        text = "Longitud de la cola: ";
+        break;
+      case "wq":
+        text = "Tiempo de espera en la cola: ";
+        break;
+      case "ls":
+        text = "Longitud del sistema: ";
+        break;
+      case "ws":
+        text = "Tiempo de espera en el sistema: ";
+        break;
+      case "u":
+        text = "Utilización del sistema: ";
+        break;
+    }
+    resultado.innerHTML += `<div id="${property}">${text} ${results[property]}</div>`;
+  }
 }
-function alert() {
+function question() {
   Swal.fire({
     title: "Tienes los promedios de los datos?",
     icon: "question",
@@ -21,9 +56,8 @@ function alert() {
     cancelButtonText: "No",
   }).then((result) => {
     if (result.isConfirmed) {
-      document.location.href = "oneOne.html";
+      document.getElementById("promedio").style.display = "block";
     } else {
-      document.location.href = "oneOne.html";
     }
   });
 }
